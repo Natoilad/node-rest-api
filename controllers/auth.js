c84b096e3250c3d852486672a66142d1bb440e8c
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-05-26 00:35:04 +0300

    add status to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
   await User.findByIdAndUpdate(user._id, { token });
 
-  res.json({
+  res.status(200).json({
     token: token,
     user: { email: user.email, subscription: user.subscription },
   });
