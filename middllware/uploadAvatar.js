ac442fe9ea57f7d7c3a87171fbc6b3bf565e44ed
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-05-31 01:36:44 +0300

    base complete

--- middllware/uploadAvatar.js
+++ middllware/uploadAvatar.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const path = require('path');
 
 const tmpDir = path.join(__dirname, '../', 'tmp');
-
 const multerConfig = multer.diskStorage({
   destination: tmpDir,
   filename: (req, file, cb) => {
